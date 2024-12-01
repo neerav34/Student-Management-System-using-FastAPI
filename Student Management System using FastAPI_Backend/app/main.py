52852c862bee41c7ee4cb63ed3f1d444906bb7ea
Neerav Jha <113015607+neerav34@users.noreply.github.com>
2024-12-01 15:52:30 +0530

    Update main.py

--- Student Management System using FastAPI_Backend/app/main.py
+++ Student Management System using FastAPI_Backend/app/main.py
@@ -1,14 +1,3 @@
-# from fastapi import FastAPI
-# from app.routes import router
-
-# app = FastAPI(
-#     title="Backend Intern Hiring Task",
-#     description="APIs for Student Management System",
-#     version="1.0.0",
-# )
-
-# app.include_router(router, prefix="/students", tags=["Students"])
-
 from fastapi import FastAPI
 from app.routers import student_router
 
