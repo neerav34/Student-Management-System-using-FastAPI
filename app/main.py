be68cf67222e7e3681fae22820a3d9d74c72ca93
Neerav Jha <113015607+neerav34@users.noreply.github.com>
2024-12-01 20:51:56 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,5 +3,8 @@ from fastapi import FastAPI
 from app.routers import student_router
 
 app = FastAPI()
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to my FastAPI app!"}
 
 app.include_router(student_router.router, prefix="/students", tags=["students"])
