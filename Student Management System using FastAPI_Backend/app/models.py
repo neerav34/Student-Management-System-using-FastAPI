af779644678c715ad11367cbda103825c68fd572
Neerav Jha <113015607+neerav34@users.noreply.github.com>
2024-12-01 15:52:42 +0530

    Update models.py

--- Student Management System using FastAPI_Backend/app/models.py
+++ Student Management System using FastAPI_Backend/app/models.py
@@ -1,25 +1,3 @@
-# from pydantic import BaseModel, Field
-# from typing import Optional
-
-
-# # Request body for creating/updating a student
-# class Address(BaseModel):
-#     city: str = Field(..., example="New York")
-#     country: str = Field(..., example="USA")
-
-
-# class Student(BaseModel):
-#     name: str = Field(..., example="John Doe")
-#     age: int = Field(..., example=20)
-#     address: Address
-
-
-# class UpdateStudent(BaseModel):
-#     name: Optional[str]
-#     age: Optional[int]
-#     address: Optional[Address]
-
-
 from pydantic import BaseModel
 from typing import Optional
 
